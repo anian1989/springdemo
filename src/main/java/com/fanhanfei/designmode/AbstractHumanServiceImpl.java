7f9b38edff77deafe135ee5aaad61b3b53451876
zhangjunshuai <zhangjunshuai@gotokeep.com>
2019-09-09 14:52:12 +0800

    WIP(dev): 抽象类和接口
    1、测试抽象类部分实现接口
    2、增加logback的配置

---
+++ src/main/java/com/fanhanfei/designmode/AbstractHumanServiceImpl.java
@@ -0,0 +1,9 @@
+package com.fanhanfei.designmode;
+
+/**
+ * @author zhangjunshuai
+ * @date 2019/9/9
+ * @description
+ **/
+public class AbstractHumanServiceImpl {
+}
---
+++ src/test/java/com/fanhanfei/springdemo/DesignModeTest.java
@@ -0,0 +1,9 @@
+package com.fanhanfei.springdemo;
+
+/**
+ * @author zhangjunshuai
+ * @date 2019/9/9
+ * @description
+ **/
+public class DesignModeTest {
+}
