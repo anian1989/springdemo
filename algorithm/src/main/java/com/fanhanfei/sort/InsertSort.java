c8ee127f036c14577b957ee50a31d0159540f2d6
zhangjunshuai <zhangjunshuai@gotokeep.com>
2020-01-02 19:12:16 +0800

    插入排序-增加新方式

--- algorithm/src/main/java/com/fanhanfei/sort/InsertSort.java
+++ algorithm/src/main/java/com/fanhanfei/sort/InsertSort.java
@@ -5,11 +5,25 @@ import java.util.List;
 /**
  * @author zhangjunshuai
  * @date 2020/1/2
- * @description
+ * @description 插入排序
+ * 有两种方式
  **/
 public class InsertSort<E extends Comparable> extends AbstractBaseSort<E>{
     @Override
-    public List<E> sortImp(List<E> datalist) {
+    public List<E> sortImp(List<E> dataList) {
+        int size = dataList.size();
+        for (int i = 1; i < size; i++) {
+            E underMindedE = dataList.get(i);
+            int j = i-1;
+            for (; j >-1&&underMindedE.compareTo(dataList.get(j))<0 ; j--) {
+                dataList.set(j+1,dataList.get(j));
+            }
+            dataList.set(j+1,underMindedE);
+        }
+        return dataList;
+    }
+
+    public List<E> sortImpV1(List<E> datalist) {
         int size = datalist.size();
         for (int i = 1; i < size; i++) {
             E undermindObject = datalist.get(i);
