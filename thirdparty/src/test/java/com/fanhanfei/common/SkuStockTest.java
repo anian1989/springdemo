d6491b0ff934c40ea730bab66e94c6796fb9f221
zhangjunshuai <zhangjunshuai@gotokeep.com>
2020-01-06 18:22:45 +0800

    work:add 调拨

--- thirdparty/src/test/java/com/fanhanfei/common/SkuStockTest.java
+++ thirdparty/src/test/java/com/fanhanfei/common/SkuStockTest.java
@@ -32,7 +32,7 @@ public class SkuStockTest {
     @Before
     public void BeforeTest(){
         log.info("测试前初始化……");
-        HttpClient.COOKIE_STR="_ga=GA1.2.1065160545.1536546797; x-app-platform=other; x-user-id.sig=8D0ZvX9TcTs5K0jTfjstU20vpcI; x-user-id=; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2256f615b988ee97882b7e953c%22%2C%22%24device_id%22%3A%22165c1537765319-0a573bfe272fa7-34647908-1024000-165c1537766872%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_utm_source%22%3A%22moment%22%2C%22%24latest_utm_medium%22%3A%22web%22%2C%22%24latest_utm_campaign%22%3A%22client_share%22%2C%22%24latest_utm_content%22%3A%22topic%22%7D%2C%22first_id%22%3A%22165c1537765319-0a573bfe272fa7-34647908-1024000-165c1537766872%22%7D; sreAuth=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InpoYW5nanVuc2h1YWkiLCJleHAiOjE1NzQwMDYyMDJ9.0dt4O1R7_5kdRFmF9JFPv-BGIR51MK73WdIl0_5_6YE; JSESSIONID=72D1747761FBB7AD1A041CFB8BDC198C; onlineLdapAuth=Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiemhhbmdqdW5zaHVhaSIsImlhdCI6MTU3Nzk0OTE0NiwiZXhwIjoxNTc4MTIxOTQ2fQ.6kn4vftomKqlaz7QSyMyuLqgN9Mv-LeRns0DwgpIlI8; _gid=GA1.2.176084953.1578023976; x-request-id=db85241123e9bfbe3cf19934acee3e1d; x-request-id.sig=d_tz-WXbLdrrW7Yv-1uZ9xTPmSE; x-app-platform.sig=GsyqGlAj3nNnT9NjWRRIVidZyos";
+        HttpClient.COOKIE_STR="_ga=GA1.2.1065160545.1536546797; x-app-platform=other; x-user-id.sig=8D0ZvX9TcTs5K0jTfjstU20vpcI; x-user-id=; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2256f615b988ee97882b7e953c%22%2C%22%24device_id%22%3A%22165c1537765319-0a573bfe272fa7-34647908-1024000-165c1537766872%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_utm_source%22%3A%22moment%22%2C%22%24latest_utm_medium%22%3A%22web%22%2C%22%24latest_utm_campaign%22%3A%22client_share%22%2C%22%24latest_utm_content%22%3A%22topic%22%7D%2C%22first_id%22%3A%22165c1537765319-0a573bfe272fa7-34647908-1024000-165c1537766872%22%7D; sreAuth=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InpoYW5nanVuc2h1YWkiLCJleHAiOjE1NzQwMDYyMDJ9.0dt4O1R7_5kdRFmF9JFPv-BGIR51MK73WdIl0_5_6YE; onlineLdapAuth=Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiemhhbmdqdW5zaHVhaSIsImlhdCI6MTU3Nzk0OTE0NiwiZXhwIjoxNTc4MTIxOTQ2fQ.6kn4vftomKqlaz7QSyMyuLqgN9Mv-LeRns0DwgpIlI8; x-request-id=db85241123e9bfbe3cf19934acee3e1d; x-request-id.sig=d_tz-WXbLdrrW7Yv-1uZ9xTPmSE; x-app-platform.sig=GsyqGlAj3nNnT9NjWRRIVidZyos; JSESSIONID=323E5465A2509833F36657FB05B54804";
         HttpClient.CONTENT_TYPE="application/x-www-form-urlencoded; charset=UTF-8";
         log.info("测试前初始化……end");
     }
@@ -183,7 +183,83 @@ public class SkuStockTest {
     }
 
 
+    /**
+     * 渠道调拨
+     */
+    @Test
+    public void transfer(){
+        log.info("开始测试");
+        String url="https://erp.gotokeep.com/sku/channel/flip";
+        String path="/Users/zhangjunshuai/库存差异查询/良品/渠道调拨.csv";
+        int updateSkuStockPatternCode = 2;//调拨模式
+        List<String> contentFromCsv = FileReadUtils.getContentFromCsv(path);
+        if (CollectionUtils.isNotEmpty(contentFromCsv)) {
+            ArrayList<String> successList = Lists.newArrayList();
+            ArrayList<String> failList = Lists.newArrayList();
+            try {
+                for (String line : contentFromCsv) {
+                    Thread.sleep(1000L);
+                    String[] split = line.replaceAll("\"","").split(",");
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("skuId=").append(split[0]);
+                    stringBuilder.append("&fromChannelId=").append(split[1]);
+                    stringBuilder.append("&toChannelId=").append(split[2]);
+                    stringBuilder.append("&physicalWareId=").append(split[3]);
+                    stringBuilder.append("&busFlag=0");
+                    stringBuilder.append("&step=").append(split[4]);
+                    stringBuilder.append("&updateSkuStockPatternCode=").append(updateSkuStockPatternCode);
 
+                    String postParam=stringBuilder.toString();
+                    String s = null;
+                    try {
+                        s = HttpClient.sendPost(url,postParam);
+                        log.info("调用接口的结果是:{}",s);
+                    } catch (Exception e) {
+                        log.error("调用接口异常",e);
+                    }
+                    //{"object":true,"errorCode":0,"message":null,"success":true}
+                    Map map = JSONObject.parseObject(s, Map.class);
+                    Boolean status = (Boolean)map.get("object");
+                    map.put("skuId",split[0]);
+                    map.put("physicalWareId",split[3]);
+                    String string = JSONObject.toJSONString(map);
+                    log.info("调用接口的结果是:{}",string);
+                    if (status){
+                        successList.add(string);
+                    }else {
+                        failList.add(string);
+                    }
+
+                }
+            } catch (Exception e) {
+                log.error("执行接口异常",e);
+            } finally {
+                System.out.println("成功");
+                StringBuilder stringBuilder = new StringBuilder();
+                successList.forEach(s -> {
+                    System.out.println(s);
+                    stringBuilder.append(s).append("\n");
+                });
+                System.out.println("失败");
+                stringBuilder.append("\n失败\n");
+                failList.forEach(s -> {
+                    System.out.println(s);
+                    stringBuilder.append(s).append("\n");
+                });
+                String fileName = "/Users/zhangjunshuai/库存差异查询/调拨差执行结果"+System.currentTimeMillis()+".txt";
+                File file = new File(fileName);
+                //还需处理路径不存在的问题
+                try {
+                    Files.write(stringBuilder.toString(), file, Charsets.UTF_8);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else {
+            log.error("文件内容是空");
+        }
+
+    }
 
 
 
