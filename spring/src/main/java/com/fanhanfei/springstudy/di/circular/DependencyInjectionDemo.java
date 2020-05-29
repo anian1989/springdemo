cc2eae14da1b7a356b81f7a1c44199880b2992f5
zhangjunshuai <zhangjunshuai@gotokeep.com>
2020-05-29 17:23:37 +0800

    Spring-DI:fix

--- spring/src/main/java/com/fanhanfei/springstudy/di/circular/DependencyInjectionDemo.java
+++ spring/src/main/java/com/fanhanfei/springstudy/di/circular/DependencyInjectionDemo.java
@@ -14,15 +14,15 @@ public class DependencyInjectionDemo {
         String[] locations = {"di/di-set.xml","di/di-construct.xml","di/di-factorymethod.xml"};
         classPathXmlApplicationContext.setConfigLocations(locations);
         classPathXmlApplicationContext.refresh();
-        /*// 通过Set的方式注入
+        // # 通过Set的方式注入
         ExampleBean exampleBean = (ExampleBean)classPathXmlApplicationContext.getBean("exampleBean");
         System.out.println(exampleBean);
 
-        // 通过构造方法注入
+        // # 通过构造方法注入
         ExampleConstructBean exampleConstructBean = (ExampleConstructBean) classPathXmlApplicationContext.getBean("exampleConstructBean");
-        System.out.println(exampleConstructBean);*/
+        System.out.println(exampleConstructBean);
 
-        // 通过工厂方法创建
+        // # 通过工厂方法创建,注意入参的配置
         ExampleWithFactoryBean exampleWithFactoryBean = (ExampleWithFactoryBean)classPathXmlApplicationContext.getBean("exampleWithFactoryBean");
         System.out.println(exampleWithFactoryBean);
         classPathXmlApplicationContext.close();
